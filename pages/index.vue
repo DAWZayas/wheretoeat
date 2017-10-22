c86d8dada1d3eca5171da210cd58ba8e008632be
Javier Pérez <surtich@gmail.com>
2017-10-23 01:16:31 +0200

    Added Tomato splash screen component

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <pomodoro v-if="isAuthenticated"></pomodoro>
-    <login v-if="!isAuthenticated"></login>
+    <div v-if="isLoading"><tomato /></div>
+    <div v-if="!isLoading">
+      <pomodoro v-if="isAuthenticated"></pomodoro>
+      <login v-if="!isAuthenticated"></login>
+    </div>
   </div>
 </template>
 <script>
   import login from '~/pages/login'
   import pomodoro from '~/pages/pomodoro'
-  
+  import Tomato from '~/components/common/Tomato'
+
   export default {
     data () {
       return {
+        isLoading: true,
         isAuthenticated: false
       }
     },
-    components: {login, pomodoro}
+    components: {login, pomodoro, Tomato},
+    beforeCreate () {
+      setTimeout(() => {
+        this.isLoading = false
+      }, 2000)
+    }
   }
 </script>
