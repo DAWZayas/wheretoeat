b480a0efb3eb1fa68d85d821e451b1edf262e239
Javier Pérez <surtich@gmail.com>
2017-10-23 11:49:35 +0200

    Added actions to setup config settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <header-component></header-component>
-    Settings
+    <div class="container min-full-height">
+      <div class="main-content row">
+        <div class="col-sm-12 col-lg-8">
+          <pomodoro-timer-settings></pomodoro-timer-settings>
+        </div>
+      </div>
+    </div>
     <footer-component></footer-component>
   </div>
 </template>
 <script>
   import { HeaderComponent, FooterComponent } from '~/components/common'
+  import PomodoroTimerSettings from '~/components/settings/PomodoroTimerSettings'
 
   export default {
     components: {
+      HeaderComponent,
       FooterComponent,
-      HeaderComponent
+      PomodoroTimerSettings
     }
   }
 </script>
-<style lang="scss">
-</style>
--- store/actions.js
+++ store/actions.js
@@ -1,1 +1,38 @@
-export default {}
+export default {
+    /**
+   * Sets the working pomodoro timer
+   * @param {object} store
+   * @param {number} workingPomodoro
+   */
+  setWorkingPomodoro ({commit, state}, workingPomodoro) {
+    if (!workingPomodoro) {
+      return
+    }
+    workingPomodoro = parseFloat(workingPomodoro)
+    commit('setWorkingPomodoro', workingPomodoro)
+  },
+  /**
+   * Sets the short break pomodoro configuration
+   * @param {object} store
+   * @param {number} shortBreak
+   */
+  setShortBreak ({commit, state}, shortBreak) {
+    if (!shortBreak) {
+      return
+    }
+    shortBreak = parseFloat(shortBreak)
+    commit('setShortBreak', shortBreak)
+  },
+  /**
+   * Sets the long break pomodoro configuration
+   * @param {object} store
+   * @param {number} longBreak
+   */
+  setLongBreak ({commit, state}, longBreak) {
+    if (!longBreak) {
+      return
+    }
+    longBreak = parseFloat(longBreak)
+    commit('setLongBreak', longBreak)
+  }
+}
